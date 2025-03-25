2646df5185bd5a316d878ab2d8e5d88719e1d79f
Rocket457 <71684147+Rocket457@users.noreply.github.com>
2025-03-25 20:59:35 -0300

    commit

--- hemograph-web/src/services/api.js
+++ hemograph-web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: process.env.URL_BACKEND
 });
 
 // Interceptor para adicionar o token de autenticação
