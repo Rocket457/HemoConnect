cac755cdd3c9a7c5731d25a857f423001be1956b
Rocket457 <71684147+Rocket457@users.noreply.github.com>
2025-03-25 22:06:27 -0300

    commit

--- server.js
+++ server.js
@@ -9,23 +9,8 @@ const necessidadesSangueRoutes = require('./routes/necessidadesSangue');
 
 const app = express();
 
-// Configuração do CORS
-const corsOptions = {
-  origin: [
-    'https://hemoconnectbackend.vercel.app/',
-    'https://hemoconnectbackend.vercel.app/api-docs',
-    'https://hemo-connect-qgu8-git-main-rocket457s-projects.vercel.app',
-    'https://hemo-connect.vercel.app',
-    'http://localhost:3000'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  maxAge: 86400 // 24 horas
-};
-
 // Middleware
-app.use(cors(corsOptions));
+app.use(cors()); // Permite todas as origens
 app.use(express.json());
 
 // Configuração do Swagger
